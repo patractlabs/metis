297d5dd7307ef0c9ebb79f3aa36326a2863608d5
clearloop <udtrokia@gmail.com>
2020-12-20 12:26:40 +0800

    feat(groth16/api): adapt mutiple params api

--- groth16/lib.rs
+++ groth16/lib.rs
@@ -25,23 +25,47 @@ mod groth16 {
         }
 
         #[ink(message)]
-        pub fn bls12_377_verify(&self, parcel: Vec<u8>) -> Result<bool, String> {
-            curve::verify(0x00, parcel).map_err(|e| e.debug())
+        pub fn bls12_377_verify(
+            &self,
+            vk_gamma_abc: Vec<Vec<u8>>,
+            vk: Vec<u8>,
+            proof: Vec<u8>,
+            public_inputs: Vec<Vec<u8>>,
+        ) -> Result<bool, String> {
+            curve::verify(0x00, vk_gamma_abc, vk, proof, public_inputs).map_err(|e| e.debug())
         }
 
         #[ink(message)]
-        pub fn bls12_381_verify(&self, parcel: Vec<u8>) -> Result<bool, String> {
-            curve::verify(0x10, parcel).map_err(|e| e.debug())
+        pub fn bls12_381_verify(
+            &self,
+            vk_gamma_abc: Vec<Vec<u8>>,
+            vk: Vec<u8>,
+            proof: Vec<u8>,
+            public_inputs: Vec<Vec<u8>>,
+        ) -> Result<bool, String> {
+            curve::verify(0x10, vk_gamma_abc, vk, proof, public_inputs).map_err(|e| e.debug())
         }
 
         #[ink(message)]
-        pub fn bn254_verify(&self, parcel: Vec<u8>) -> Result<bool, String> {
-            curve::verify(0x20, parcel).map_err(|e| e.debug())
+        pub fn bn254_verify(
+            &self,
+            vk_gamma_abc: Vec<Vec<u8>>,
+            vk: Vec<u8>,
+            proof: Vec<u8>,
+            public_inputs: Vec<Vec<u8>>,
+        ) -> Result<bool, String> {
+            curve::verify(0x20, vk_gamma_abc, vk, proof, public_inputs).map_err(|e| e.debug())
         }
 
         #[ink(message)]
-        pub fn bw6_761(&self, parcel: Vec<u8>) -> Result<bool, String> {
-            curve::verify(0x30, parcel).map_err(|e| e.debug())
+        pub fn bw6_761(
+            &self,
+            vk_gamma_abc: Vec<Vec<u8>>,
+            vk: Vec<u8>,
+            proof: Vec<u8>,
+            public_inputs: Vec<Vec<u8>>,
+        ) -> Result<bool, String> {
+            curve::verify(0x30, vk_gamma_abc, vk, proof, public_inputs).map_err(|e| e.debug())
         }
     }
 }
