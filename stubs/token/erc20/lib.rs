06e3d5168b821ffc4846839f07299ab498b386b2
zhangtailin <tailinzhang1993@gmail.com>
2021-03-16 12:55:41 +0800

    set fixed selector for erc20 stub

--- stubs/token/erc20/lib.rs
+++ stubs/token/erc20/lib.rs
@@ -5,17 +5,28 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod erc20 {
-    use erc20_trait::{IErc20, Result};
     use ink_prelude::string::String;
 
-    /// Basic version of Erc20Stub.
+    /// The ERC-20 error types.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Returned if not enough balance to fulfill a request is available.
+        InsufficientBalance,
+        /// Returned if not enough allowance to fulfill a request is available.
+        InsufficientAllowance,
+    }
+
+    /// The ERC-20 result type.
+    pub type Result<T> = core::result::Result<T, Error>;
+
     #[ink(storage)]
     pub struct Erc20Stub {}
 
-    impl IErc20 for Erc20Stub {
+    impl Erc20Stub {
         /// Creates a new ERC-20 contract with the specified initial supply.
         #[ink(constructor)]
-        fn new(
+        pub fn new(
             _initial_supply: Balance,
             _name: Option<String>,
             _symbol: Option<String>,
@@ -25,50 +36,50 @@ mod erc20 {
         }
 
         /// Returns the token name.
-        #[ink(message)]
-        fn token_name(&self) -> Option<String> {
+        #[ink(message, selector = "0x6b1bb951")]
+        pub fn token_name(&self) -> Option<String> {
             unimplemented!()
         }
 
         /// Returns the token symbol.
-        #[ink(message)]
-        fn token_symbol(&self) -> Option<String> {
+        #[ink(message, selector = "0xb42c3368")]
+        pub fn token_symbol(&self) -> Option<String> {
             unimplemented!()
         }
 
         /// Returns the token decimals.
-        #[ink(message)]
-        fn token_decimals(&self) -> Option<u8> {
+        #[ink(message, selector = "0xc64b0eb2")]
+        pub fn token_decimals(&self) -> Option<u8> {
             unimplemented!()
         }
 
         /// Returns the total token supply.
-        #[ink(message)]
-        fn total_supply(&self) -> Balance {
+        #[ink(message, selector = "0x143862ae")]
+        pub fn total_supply(&self) -> Balance {
             unimplemented!()
         }
 
         /// Returns the account balance for the specified `owner`.
-        #[ink(message)]
-        fn balance_of(&self, _owner: AccountId) -> Balance {
+        #[ink(message, selector = "0xb7d968c9")]
+        pub fn balance_of(&self, _owner: AccountId) -> Balance {
             unimplemented!()
         }
 
         /// Transfers `value` amount of tokens from the caller's account to account `to`.
-        #[ink(message)]
-        fn transfer(&mut self, _to: AccountId, _value: Balance) -> Result<()> {
+        #[ink(message, selector = "0x10d455c2")]
+        pub fn transfer(&mut self, _to: AccountId, _value: Balance) -> Result<()> {
             unimplemented!()
         }
 
         /// Returns the amount which `spender` is still allowed to withdraw from `owner`.
-        #[ink(message)]
-        fn allowance(&self, _owner: AccountId, _spender: AccountId) -> Balance {
+        #[ink(message, selector = "0xc04aa300")]
+        pub fn allowance(&self, _owner: AccountId, _spender: AccountId) -> Balance {
             unimplemented!()
         }
 
         /// Transfers `value` tokens on the behalf of `from` to the account `to`.
-        #[ink(message)]
-        fn transfer_from(
+        #[ink(message, selector = "0xbb399017")]
+        pub fn transfer_from(
             &mut self,
             _from: AccountId,
             _to: AccountId,
@@ -79,8 +90,8 @@ mod erc20 {
 
         /// Allows `spender` to withdraw from the caller's account multiple times, up to
         /// the `value` amount.
-        #[ink(message)]
-        fn approve(&mut self, _spender: AccountId, _value: Balance) -> Result<()> {
+        #[ink(message, selector = "0x4ce0e831")]
+        pub fn approve(&mut self, _spender: AccountId, _value: Balance) -> Result<()> {
             unimplemented!()
         }
     }
