b49fbf5da023f1565d94838969cce10632673d32
fy <fanyang.coder@gmail.com>
2021-06-15 17:28:59 +0800

    ERC165 Standard Interface Detection Support (#28)
    * add a example for erc165
    
    * add test for erc165 test
    
    * support no std for env accountid
    
    * add marco api for erc165
    
    * impl erc165 marco
    
    * use metis::contract out of supports
    
    * add TODOs
    
    * add comment for macro
    
    * add test for erc165 marco

---
+++ crates/lang/codegen/src/components/erc165.rs
@@ -0,0 +1,122 @@
+use proc_macro2::{
+    Group,
+    Ident,
+    Span,
+    TokenStream as TokenStream2,
+};
+use quote::quote;
+use syn::{
+    parse::{
+        Parse,
+        ParseStream,
+        Result,
+    },
+    punctuated::Punctuated,
+    Token,
+};
+
+use super::super::utils::Args;
+
+// Parses a func with param name like Name(a, b, c)
+pub struct FuncParam {
+    pub name: Ident,
+    pub attrs: Vec<Ident>,
+}
+
+impl Parse for FuncParam {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // interface(new, default)
+        // println!("FuncParam input {:#?}", &input);
+
+        let name = input.parse::<Ident>()?;
+        // println!("FuncParam name {:#?}", &name);
+
+        let attrs_group = input.parse::<Group>()?;
+        // println!("FuncParam attrs_group {:#?}", &attrs_group);
+
+        let attrs = syn::parse2::<Args>(attrs_group.stream())?;
+
+        Ok(Self {
+            name,
+            attrs: attrs.vars.into_iter().collect(),
+        })
+    }
+}
+
+// Parses a func with param name like 'Name1(a, b, c), Name2(a, b, v)'
+pub struct FuncParams {
+    pub funcs: Vec<FuncParam>,
+}
+
+impl Parse for FuncParams {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let vars = Punctuated::<FuncParam, Token![,]>::parse_terminated(input)?;
+        Ok(FuncParams {
+            funcs: vars.into_iter().collect(),
+        })
+    }
+}
+
+fn calculate_interface_id(idents: &Vec<Ident>) -> u32 {
+    // return hash(a) ^ hash(b) ^ ...
+
+    let mut interface_id: usize = 0;
+
+    for i in idents {
+        let callable_ident = i.to_string().into_bytes();
+        let selector = ink_lang_ir::Selector::new(&callable_ident);
+        let selector_id = u32::from_be_bytes(selector.as_bytes().clone()) as usize;
+
+        interface_id = interface_id ^ selector_id;
+    }
+
+    // println!("interface_id {:#0x}", &interface_id);
+
+    interface_id as u32
+}
+
+fn generate_supports_message(func_params: FuncParams) -> Result<TokenStream2> {
+    let mut interface_ids = func_params
+        .funcs
+        .iter()
+        .filter(|f| f.name == "interface")
+        .map(|f| calculate_interface_id(&f.attrs))
+        .into_iter()
+        .collect::<Vec<_>>();
+    
+    // for supports_interface
+    interface_ids.push(calculate_interface_id(&vec![Ident::new("supports_interface", Span::call_site())]));
+
+    let match_id_trues = interface_ids
+        .iter()
+        .map(|interface_id| {
+            quote! {
+                #interface_id => true,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    Ok(quote! {
+        impl Flipper {
+            fn _supports_interface(&self, interface_id: u32) -> bool {
+                match interface_id {
+                    #( #match_id_trues )*
+                    _ => false,
+                }
+            }
+        }
+    })
+}
+
+pub fn generate_code(attr: TokenStream2, input: TokenStream2) -> Result<TokenStream2> {
+    // #[metis::supports(interface(new, default), interface(flip, get))]
+    // attr : interface(new, default), interface(flip, get)
+
+    let params = syn::parse2::<FuncParams>(attr)?;
+    let impl_codes = generate_supports_message(params)?;
+
+    Ok(quote! {
+        #input
+        #impl_codes
+    })
+}
---
+++ examples/erc165/contracts/bymarco/lib.rs
@@ -0,0 +1,114 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use metis_lang as metis;
+
+#[metis::contract]
+pub mod flipper {
+    use super::*;
+
+    #[ink(storage)]
+    pub struct Flipper {
+        value: bool,
+    }
+
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
+    pub struct Flip {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        value: bool,
+    }
+
+    #[metis::supports(interface(new, default), interface(flip, get))]
+    impl Flipper {
+        /// Creates a new flipper smart contract initialized with the given value.
+        #[ink(constructor)]
+        pub fn new(init_value: bool) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Creates a new flipper smart contract initialized to `false`.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// Return the contract is support the interface_id
+        #[ink(message)]
+        pub fn supports_interface(&self, interface_id: u32) -> bool {
+            // _supports_interface gen by marco
+            // TODO: use a common marco in metis to gen this message fn
+            self._supports_interface(interface_id)
+        }
+
+        /// Flips the current value of the Flipper's bool.
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            let caller = Self::env().caller();
+            self.value = !self.value;
+
+            Self::env().emit_event(Flip {
+                from: Some(caller),
+                value: self.value,
+            });
+        }
+
+        /// Flips set the current value
+        #[ink(message)]
+        pub fn set(&mut self, value: bool) {
+            let caller = Self::env().caller();
+            self.value = value;
+
+            Self::env().emit_event(Flip {
+                from: Some(caller),
+                value: self.value,
+            });
+        }
+
+        /// Returns the current value of the Flipper's bool.
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn default_works() {
+            let flipper = Flipper::default();
+            assert_eq!(flipper.get(), false);
+        }
+
+        #[ink::test]
+        fn it_works() {
+            let mut flipper = Flipper::new(false);
+            assert_eq!(flipper.get(), false);
+            flipper.flip();
+            assert_eq!(flipper.get(), true);
+        }
+
+        #[ink::test]
+        fn support_works() {
+            let mut flipper = Flipper::new(false);
+            assert_eq!(flipper.get(), false);
+            flipper.flip();
+            assert_eq!(flipper.get(), true);
+        }
+
+        #[ink::test]
+        fn set_works() {
+            let mut flipper = Flipper::new(false);
+            assert_eq!(flipper.get(), false);
+
+            flipper.set(true);
+            assert_eq!(flipper.get(), true);
+
+            flipper.set(false);
+            assert_eq!(flipper.get(), false);
+        }
+    }
+}
