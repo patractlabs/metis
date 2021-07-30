540818399bb83abebc814efb2b00996adfecea29
fanyang1988 <fanyang.coder@gmail.com>
2021-07-30 14:38:16 +0800

    add ownable test

--- example/tests/access-control/ownable.test.ts
+++ example/tests/access-control/ownable.test.ts
@@ -5,38 +5,45 @@ import type { Text } from '@polkadot/types';
 
 const { getContractFactory, getRandomSigner } = patract;
 
-const { api, getSigners } = network;
+const { api, getAddresses, getSigners } = network;
+
+
+async function expectRevert(promise, expectedError) {
+  try {
+    await promise;
+  } catch (exp) {
+    expect(exp.error.message).to.equal("contracts.ContractTrapped( Contract trapped during execution.)")
+  }
+}
 
 describe('ERC20', () => {
-  after(() => {
-    return api.disconnect();
-  });
-
   async function setup() {
     const one = new BN(10).pow(new BN(api.registry.chainDecimals[0]));
     const signers = await getSigners();
+    const addresses = await getAddresses();
     const Alice = signers[0];
     const sender = await getRandomSigner(Alice, one.muln(10000));
     const contractFactory = await getContractFactory('ownable_erc20', sender);
     const contract = await contractFactory.deploy('new', '1000');
     const abi = artifacts.readArtifact('ownable_erc20');
-    const receiver = await getRandomSigner();
+    const receiver = addresses[1];
+
+    await api.tx.balances.transfer(receiver, 50000000000).signAndSend(addresses[0]);
 
     return { sender, contractFactory, contract, abi, receiver, Alice, one };
   }
-
 
   it('Transfer emits event', async () => {
     const { contract, sender, receiver } = await setup();
 
     console.log("contract ", contract.address.toHex())
     console.log("sender", sender.address)
-    console.log("receiver", receiver.address)
+    console.log("receiver", receiver)
 
 
-    await expect(contract.tx.transfer(receiver.address, 7))
+    await expect(contract.tx.transfer(receiver, 7))
       .to.emit(contract, 'Transfer')
-      .withArgs(sender.address, receiver.address, 7);
+      .withArgs(sender.address, receiver, 7);
   });
 
   it('ERC20 metadatas', async () => {
@@ -52,5 +59,53 @@ describe('ERC20', () => {
     await expect(tokenDecimals.output).to.equal(18);
   });
 
+  it('Owner initstate', async () => {
+    const { contract, sender, receiver } = await setup();
 
+    await expect((await contract.query.getOwnership()).output).to.equal(sender.address);
+  });
+
+  it('Owner renounce', async () => {
+    const { contract, sender, receiver } = await setup();
+
+    context('owner initial is ok', async function () {
+      await expect((await contract.query.getOwnership()).output).to.equal(sender.address);
+    });
+
+    context('owner renounce not owner will failed', async function () {
+      await expectRevert(
+        contract.connect(receiver).tx.renounceOwnership(), '',
+      );
+
+      await expect((await contract.query.getOwnership()).output).to.equal(sender.address);
+    });
+
+    context('owner renounce by owner will ok', async function () {
+      await contract.connect(sender.address).tx.renounceOwnership();
+
+      await expect((await contract.query.getOwnership()).output).to.equal(null);
+    });
+  });
+
+  it('Owner transfer_ownership', async () => {
+    const { contract, sender, receiver } = await setup();
+
+    context('owner initial is ok', async function () {
+      await expect((await contract.query.getOwnership()).output).to.equal(sender.address);
+    });
+
+    context('owner transfer_ownership not owner will failed', async function () {
+      await expectRevert(
+        contract.connect(receiver).tx.transferOwnership(receiver), '',
+      );
+
+      await expect((await contract.query.getOwnership()).output).to.equal(sender.address);
+    });
+
+    context('owner transfer_ownership by owner will ok', async function () {
+      await contract.connect(sender.address).tx.transferOwnership(receiver);
+
+      await expect((await contract.query.getOwnership()).output).to.equal(receiver);
+    });
+  });
 });
