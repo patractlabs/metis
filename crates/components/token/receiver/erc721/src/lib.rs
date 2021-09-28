f911775ea3388a3265ddd8e3e47fd70421b0e1a2
fy <fanyang.coder@gmail.com>
2021-09-28 10:52:02 +0800

    add the receiver for erc721 and erc1155 (#55)
    * add the receiver for erc721 and erc1155
    
    * fix build error and add tests

---
+++ crates/components/token/receiver/erc721/src/lib.rs
@@ -0,0 +1,50 @@
+//! Contract components which provides a common implementation for erc721 receiver.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_prelude::vec::Vec;
+use metis_erc721::TokenId;
+use metis_lang::{
+    Env,
+    EnvAccess,
+    Storage,
+};
+
+#[cfg(not(feature = "ink-as-dependency"))]
+use ::ink_storage::traits::SpreadLayout;
+
+#[cfg_attr(feature = "std", derive(::ink_storage::traits::StorageLayout))]
+#[derive(Debug, SpreadLayout, Default)]
+pub struct Data {}
+
+/// The `EventEmit` impl the event emit api for erc721-receiver component.
+pub trait EventEmit<E: Env>: EnvAccess<E> {
+    /// Emit Erc721Received event
+    fn emit_event_erc_721_received(
+        &mut self,
+        operator: E::AccountId,
+        from: E::AccountId,
+        token_id: TokenId,
+        data: Vec<u8>,
+    );
+}
+
+/// The `Impl` define erc721-receiver component impl funcs
+pub trait Impl<E: Env>: EventEmit<E> + Storage<E, Data> {
+    /// init Initializes the contract setting the deployer as the initial owner.
+    fn init(&mut self) {}
+
+    fn on_erc721_received(
+        &mut self,
+        operator: E::AccountId,
+        from: E::AccountId,
+        token_id: TokenId,
+        data: Vec<u8>,
+    ) -> [u8; 4] {
+        Self::emit_event_erc_721_received(self, operator, from, token_id, data);
+
+        metis_lang::selector_id!(on_erc721_received)
+    }
+}
+
+impl<E: Env, T: EventEmit<E> + Storage<E, Data>> Impl<E> for T {}
--- example/contracts/token/erc1155-token-receiver/lib.rs
+++ example/contracts/token/erc1155-token-receiver/lib.rs
@@ -2,16 +2,14 @@
 
 #[metis_lang::contract]
 pub mod contract {
-    use ink_prelude::{
-        vec::Vec,
-        vec,
-    };
+    use ink_prelude::vec::Vec;
     use metis_erc721::TokenId;
     use metis_lang::{
         import,
         metis,
     };
     use metis_ownable as ownable;
+    use metis_receiver_erc1155 as erc1155_receiver;
 
     #[cfg(not(feature = "ink-as-dependency"))]
     use ::ink_storage::{
@@ -20,16 +18,18 @@ pub mod contract {
     };
 
     #[ink(storage)]
-    #[import(ownable)]
+    #[import(ownable, erc1155_receiver)]
     pub struct Erc1155Receiver {
         ownable: ownable::Data<Erc1155Receiver>,
+        erc1155_receiver: erc1155_receiver::Data,
 
         is_receive: Lazy<bool>,
-        erc1155_receive: StorageHashMap<(AccountId, TokenId), ()>,
+        receivers: StorageHashMap<(AccountId, TokenId), ()>,
     }
 
     /// Emitted when contract is received the erc1155 `token_id` token is transferred from `from` to `to`.
     #[ink(event)]
+    #[metis(erc1155_receiver)]
     pub struct Erc1155Received {
         #[ink(topic)]
         pub operator: AccountId,
@@ -66,9 +66,10 @@ pub mod contract {
         pub fn default() -> Self {
             Self {
                 ownable: ownable::Data::new(),
+                erc1155_receiver: erc1155_receiver::Data::default(),
 
                 is_receive: Lazy::new(false),
-                erc1155_receive: StorageHashMap::new(),
+                receivers: StorageHashMap::new(),
             }
         }
 
@@ -83,14 +84,14 @@ pub mod contract {
         pub fn add_accept_token(&mut self, contract: AccountId, id: TokenId) {
             ownable::Impl::ensure_caller_is_owner(self);
 
-            self.erc1155_receive.insert((contract, id), ());
+            self.receivers.insert((contract, id), ());
         }
 
         #[ink(message)]
         pub fn del_accept_token(&mut self, contract: AccountId, id: TokenId) {
             ownable::Impl::ensure_caller_is_owner(self);
 
-            self.erc1155_receive.take(&(contract, id));
+            self.receivers.take(&(contract, id));
         }
 
         #[ink(message)]
@@ -122,16 +123,10 @@ pub mod contract {
                 "ERC721Receiver: Current not receive erc721 token"
             );
 
-            if self.erc1155_receive.contains_key(&(Self::env().caller(), id)) {
-                Self::env().emit_event(Erc1155Received {
-                    operator,
-                    from,
-                    ids: vec![id],
-                    values: vec![value],
-                    data,
-                });
-
-                [194u8, 238u8, 217u8, 152u8]
+            if self.receivers.contains_key(&(Self::env().caller(), id)) {
+                erc1155_receiver::Impl::on_erc1155_received(
+                    self, operator, from, id, value, data,
+                )
             } else {
                 [0u8, 0u8, 0u8, 0u8]
             }
@@ -151,7 +146,10 @@ pub mod contract {
                 "ERC721Receiver: Current not receive erc721 token"
             );
 
-            if self.erc1155_receive.contains_key(&(Self::env().caller(), TokenId::default())) {
+            if self
+                .receivers
+                .contains_key(&(Self::env().caller(), TokenId::default()))
+            {
                 Self::env().emit_event(Erc1155Received {
                     operator,
                     from,
@@ -160,7 +158,7 @@ pub mod contract {
                     data,
                 });
 
-                [22u8, 32u8, 73u8, 133u8]
+                metis_lang::selector_id!(on_erc1155_batch_received)
             } else {
                 [0u8, 0u8, 0u8, 0u8]
             }
